ac44736a94fa39edde865067294b7ab1509496e4
nhattpam <nhat2up@gmail.com>
2024-01-24 14:51:46 +0700

    lopl

--- EPLCNL_Backend/EPLCNL-API/Controllers/AuthenticationsController.cs
+++ EPLCNL_Backend/EPLCNL-API/Controllers/AuthenticationsController.cs
@@ -48,7 +48,8 @@ namespace EPLCNL_API.Controllers
                     var adminModel = new AccountResponse
                     {
                         Email = _configuration["Admin:Email"],
-                        Password = _configuration["Admin:Password"]
+                        Password = _configuration["Admin:Password"],
+                        RoleId = new Guid("5F9A0E31-E7B2-417B-917D-111468A18A53")
                     };
                     var token = GenerateToken(adminModel);
 
