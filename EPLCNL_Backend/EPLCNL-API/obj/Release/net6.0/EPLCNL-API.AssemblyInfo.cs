180f36c291c5bdad6b0b19c52428a82544bc4c46
nhattpam <nhat2up@gmail.com>
2024-01-31 13:02:04 +0700

    vnpay first

--- EPLCNL_Backend/EPLCNL-API/obj/Release/net6.0/EPLCNL-API.AssemblyInfo.cs
+++ EPLCNL_Backend/EPLCNL-API/obj/Release/net6.0/EPLCNL-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPLCNL-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2fecb481a9bdaa18c5ef7385e548b5f3a2856e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f3d88b197c6aa88644e70ca5fc3870f462c28f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPLCNL-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPLCNL-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
