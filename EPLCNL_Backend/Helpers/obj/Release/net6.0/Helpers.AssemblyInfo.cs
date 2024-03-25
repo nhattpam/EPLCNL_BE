003b69e99f0c6088f4c4c005dbcbe0fffbe1fa20
Nhat Pham <nhat2up@gmail.com>
2024-03-25 08:54:49 +0700

    db

--- EPLCNL_Backend/Helpers/obj/Release/net6.0/Helpers.AssemblyInfo.cs
+++ EPLCNL_Backend/Helpers/obj/Release/net6.0/Helpers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bdede93c421e345985c35f08ee544dcb6efab4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d23c37e62dbb406a61806a032a21759f1f6ef7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
