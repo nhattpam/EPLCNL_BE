df763d280726751fa982b660715083d859d6ee01
Nhat Pham <nhat2up@gmail.com>
2024-04-16 09:08:21 +0700

    lol

--- EPLCNL_Backend/Helpers/obj/Debug/net6.0/Helpers.AssemblyInfo.cs
+++ EPLCNL_Backend/Helpers/obj/Debug/net6.0/Helpers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ebf2eac6114dafc4d7f248ae9ebd28749f3959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+331fdc62050b6c847bcc2239097a31c2d658c87a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
