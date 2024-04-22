57dfadbaaf3c155e4ee73a5b6cc4995615d2f180
Nhat Pham <nhat2up@gmail.com>
2024-04-22 10:15:36 +0700

    change db ip

--- EPLCNL_Backend/ViewModel/obj/Debug/net6.0/ViewModel.AssemblyInfo.cs
+++ EPLCNL_Backend/ViewModel/obj/Debug/net6.0/ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a41de91ab573e4ee5348d49bab8e69767e38b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b62352c34f4f78791afea61e7c64e48c0e634c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
