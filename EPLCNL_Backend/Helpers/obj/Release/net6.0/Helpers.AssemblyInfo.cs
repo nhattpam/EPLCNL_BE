22553edd957810166802b698fd114ed3a6e0f0e4
Nhat Pham <nhat2up@gmail.com>
2024-04-24 20:14:08 +0700

    remove timeout

--- EPLCNL_Backend/Helpers/obj/Release/net6.0/Helpers.AssemblyInfo.cs
+++ EPLCNL_Backend/Helpers/obj/Release/net6.0/Helpers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57dfadbaaf3c155e4ee73a5b6cc4995615d2f180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bdde996db598261c565e788821ab32700cfa8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helpers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EPLCNL_Backend/ViewModel/obj/Debug/net6.0/ViewModel.AssemblyInfo.cs
+++ EPLCNL_Backend/ViewModel/obj/Debug/net6.0/ViewModel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57dfadbaaf3c155e4ee73a5b6cc4995615d2f180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bdde996db598261c565e788821ab32700cfa8b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewModel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
